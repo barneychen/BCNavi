eb078080b79bfeec89a9ed2c05f31af76c0ad086
barneychen <barney.chen@eztable.com>
2014-12-09 00:11:21 +0800

    Initial Commit

---
+++ BCNavi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BCNavi
+//
+//  Created by Barney Chen on 2014/12/9.
+//  Copyright (c) 2014年 CWH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BCNavi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BCNavi
+//
+//  Created by Barney Chen on 2014/12/9.
+//  Copyright (c) 2014年 CWH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
